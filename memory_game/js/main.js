8832e51b927b3fa8ea5eab849390990bc741e017
Jason Moon <jasonmoonjm@gmail.com>
2019-07-31 16:28:47 -0700

    created game logic x3

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen", 
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -11,10 +32,13 @@ function checkForMatch() {
 
 function flipCard(cardId) {
 	checkForMatch();
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 };
 
 flipCard(0); 
+console.log(cards[0].cardImage)
+console.log(cards[0].rank)
 flipCard(2);
-flipCard(3);
+console.log(cards[2].cardImage)
+console.log(cards[2].rank)

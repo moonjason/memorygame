cac896c6b030823f5c87a79e2f1e9af67c2f4b03
Jason Moon <jasonmoonjm@gmail.com>
2019-08-02 14:34:29 -0700

    createBoard function attempt

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,6 +23,7 @@ var cards = [
 var cardsInPlay = [];
 
 function checkForMatch() {
+	this.setAttribute('src', cards[cardId].cardImage)
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!"); 
 	}	else {
@@ -30,15 +31,27 @@ function checkForMatch() {
 	}
 };
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id')
 	checkForMatch();
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 };
 
-flipCard(0); 
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', "images/back.png");
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementsByTagName('#game-board')[0].appendChild(cardElement);
+	}
+}
+
 console.log(cards[0].cardImage)
 console.log(cards[0].rank)
-flipCard(2);
+
 console.log(cards[2].cardImage)
 console.log(cards[2].rank)
+
+createBoard();
